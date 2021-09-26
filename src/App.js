07a89e93a5721b383c92abd3689d819df6208bef
Falak Amin <falakamin2000@gmail.com>
2021-09-26 19:03:54 +0500

    Installed dependencies, created header, import Api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import React, { useEffect, useState } from 'react'
+import Header  from './components/Header';
+import { Container } from '@material-ui/core';
+import "./App.css";
+
 
 const App = () => {
-  return (
-    <div>
+
+const [word, setWord] = useState("")
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState("en")
+  const dictionaryApi= async() =>{
+      try
+      {
+          const data = await axios.get(
+      
+              `https://api.dictionaryapi.dev/api/v2/entries/en/plane`
+          );
+         setMeanings(data.data);
+          
+      }
+  
+  catch(error)
+  {
+      // console.log(error);
+  }
+};
+
+useEffect(() => {
+  dictionaryApi();
+
+}, []);
+return (
+
+<div className= "main-container">
+      <Container className="child-container" maxWidth="sm">
+
+      <Header/>
+      </Container>
       
     </div>
   )
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/header.css";
+
+
+const Header = () => {
+    return (
+         
+            <div className="header">
+              <span className="title">WORD OCEAN</span>
+              </div>
+    )
+}
+
+export default Header
